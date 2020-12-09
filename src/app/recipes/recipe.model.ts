c0976e8a8343a45bcdd35da74f9a65b2b2e54ddb
Niranjan Purushottam Patil <patilniru30@gmail.com>
2020-12-09 23:11:00 +0530

    completed partial template design of components

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+import { ThrowStmt } from '@angular/compiler';
+
+export class  Recipe {
+  public name : string;
+  public description : string;
+  public imagePath : string;
+  constructor(name : string, description : string, imagePath : string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
